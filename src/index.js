9826ad8f5b929f893a5e60980c63fbde6eb8df2e
antoinepomme <140323098+antoinepomme@users.noreply.github.com>
2023-08-18 01:32:10 +0200

    Add basic dialog to add todo items
    Also changed all links into buttons to fix a bug on click, it's much cleaner with buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './styles/style.css';
 import { format, compareAsc } from 'date-fns';
 import todo from './modules/todo.js';
+
+
+const addButton = document.getElementById("add");
+const dialog = document.getElementById("dialog");
+addButton.addEventListener('click', () => {
+    dialog.showModal();
+})
