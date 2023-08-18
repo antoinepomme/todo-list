a5a64908a5d182762dcb664275962c1972fe6133
antoinepomme <140323098+antoinepomme@users.noreply.github.com>
2023-08-19 00:46:47 +0200

    Add function to make todo items
    still some things to do (date handling, edit button, projects handling, ...)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles/style.css';
+import './styles/todo.css';
 import { format, compareAsc } from 'date-fns';
 import todo from './modules/todo.js';
 
+//main dialog
 const mainContainer = document.getElementById("main-container");
 const addButton = document.getElementById("add");
 const dialog = document.getElementById("dialog");
@@ -17,3 +19,12 @@ closeButton.addEventListener('click', () => {
 dialog.addEventListener('cancel', () => {
     mainContainer.style.filter = "blur(0px)";
 });
+
+
+
+//examples
+let content = document.getElementById("content");
+content.appendChild(todo("a", "1", "DATETEST", "High"));
+content.appendChild(todo("b", "2", "DATETEST", "Medium"));
+content.appendChild(todo("c", "3", "DATETEST", "High"));
+content.appendChild(todo("d", "4", "DATETEST", "Low"));
--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,3 +1,161 @@
-export default function todo(title, description, dueDate, priority) {
-    return { title, description, dueDate, priority, check: 0 };
+import trash from '../assets/icons/trash.svg';
+import edit from '../assets/icons/edit.svg';
+import cross from '../assets/icons/cross-black.svg';
+
+const mainContainerForTodo = document.getElementById("main-container");
+
+export default function todo(title, description, dueDate, priority, project) {
+    let todoItem = document.createElement("div");
+    todoItem.classList.add("todo");
+
+    todoItem.setAttribute("data-title", title);
+    if (project) {
+        todoItem.setAttribute("data-project", project);
+    } else {
+        todoItem.setAttribute("data-project", "None");
+    }
+    todoItem.setAttribute("data-priority", priority);
+    todoItem.setAttribute("data-date", dueDate);
+    todoItem.setAttribute("data-description", description);
+
+    function showDetails(element) {
+        let todoDialog = document.createElement("dialog");
+        todoDialog.classList.add("todo-dialog");
+
+        let closeDialog = document.createElement("button");
+        let crossIcon = new Image();
+        crossIcon.src = cross;
+        closeDialog.appendChild(crossIcon);
+        todoDialog.appendChild(closeDialog);
+
+        let titleDialog = document.createElement("div");
+        titleDialog.classList.add("todo-dialog-title");
+        titleDialog.textContent = element.getAttribute("data-title");
+        todoDialog.appendChild(titleDialog);
+
+        let todoDialogContainer = document.createElement("div")
+        todoDialogContainer.classList.add("todo-dialog-container")
+
+        let projectDialogTitle = document.createElement("div");
+        projectDialogTitle.classList.add("bold");
+        projectDialogTitle.textContent = "Project :";
+        todoDialogContainer.appendChild(projectDialogTitle);
+        let projectDialog = document.createElement("div");
+        projectDialog.textContent = element.getAttribute("data-project");
+        todoDialogContainer.appendChild(projectDialog);
+
+        let priorityDialogTitle = document.createElement("div");
+        priorityDialogTitle.classList.add("bold");
+        priorityDialogTitle.textContent = "Priority :";
+        todoDialogContainer.appendChild(priorityDialogTitle);
+        let priorityDialog = document.createElement("div");
+        priorityDialog.textContent = element.getAttribute("data-priority");
+        todoDialogContainer.appendChild(priorityDialog);
+
+        let dateDialogTitle = document.createElement("div");
+        dateDialogTitle.classList.add("bold");
+        dateDialogTitle.textContent = "Due Date :";
+        todoDialogContainer.appendChild(dateDialogTitle);
+        let dateDialog = document.createElement("div");
+        dateDialog.textContent = element.getAttribute("data-date");
+        todoDialogContainer.appendChild(dateDialog);
+
+        let detailsDialogTitle = document.createElement("div");
+        detailsDialogTitle.classList.add("bold");
+        detailsDialogTitle.textContent = "Description :";
+        todoDialogContainer.appendChild(detailsDialogTitle);
+        let detailsDialog = document.createElement("div");
+        detailsDialog.textContent = element.getAttribute("data-description");
+        todoDialogContainer.appendChild(detailsDialog);
+
+        todoDialog.appendChild(todoDialogContainer);
+        document.body.appendChild(todoDialog);
+
+        todoDialog.addEventListener('cancel', () => {
+            while (todoDialog.firstChild) {
+                if (closeDialog.firstChild) {
+                    closeDialog.removeChild(closeDialog.firstChild);
+                }
+                while (todoDialogContainer.firstChild) {
+                    todoDialogContainer.removeChild(todoDialogContainer.firstChild);
+                }
+                todoDialog.removeChild(todoDialog.firstChild);
+            }
+            todoDialog.close();
+            document.body.removeChild(todoDialog);
+            mainContainerForTodo.style.filter = "blur(0px)";
+        });
+
+        closeDialog.addEventListener('click', () => {
+            while (todoDialog.firstChild) {
+                if (closeDialog.firstChild) {
+                    closeDialog.removeChild(closeDialog.firstChild);
+                }
+                while (todoDialogContainer.firstChild) {
+                    todoDialogContainer.removeChild(todoDialogContainer.firstChild);
+                }
+                todoDialog.removeChild(todoDialog.firstChild);
+            }
+            todoDialog.close();
+            document.body.removeChild(todoDialog);
+            mainContainerForTodo.style.filter = "blur(0px)";
+        });
+
+        todoDialog.showModal();
+        mainContainerForTodo.style.filter = "blur(3px)";
+    }
+
+    let priorityColor = "green";
+    if (priority === "Medium") {
+        priorityColor = "orange";
+    } else if (priority === "High") {
+        priorityColor = "red";
+    }
+    todoItem.style.borderLeft = "solid " + priorityColor + " 3px";
+
+    let todoCheckBox = document.createElement("input");
+    todoCheckBox.type = "checkbox";
+    todoCheckBox.classList.add("todo-check");
+    todoItem.appendChild(todoCheckBox);
+    todoCheckBox.addEventListener('change', () => {
+        todoItem.classList.toggle("done");
+    });
+
+    let todoTitle = document.createElement("div");
+    todoTitle.classList.add("todo-title")
+    todoTitle.textContent = title;
+    todoItem.appendChild(todoTitle);
+
+    let todoDetailsButton = document.createElement("button");
+    todoDetailsButton.classList.add("todo-details")
+    todoDetailsButton.textContent = "DETAILS";
+    todoItem.appendChild(todoDetailsButton);
+    todoDetailsButton.addEventListener('click', () => {
+        showDetails(todoItem);
+    });
+
+    let todoDate = document.createElement("div");
+    todoDate.classList.add("todo-date");
+    todoDate.textContent = dueDate;
+    todoItem.appendChild(todoDate);
+
+    let todoEditButton = document.createElement("button");
+    let editIcon = new Image();
+    editIcon.src = edit;
+    todoEditButton.appendChild(editIcon);
+    todoItem.appendChild(todoEditButton);
+
+    let todoDeleteButton = document.createElement("button");
+    let editTrash = new Image();
+    editTrash.src = trash;
+    todoDeleteButton.appendChild(editTrash);
+    todoItem.appendChild(todoDeleteButton);
+    todoDeleteButton.addEventListener('click', () => {
+        while (todoItem.firstChild) {
+            todoItem.removeChild(todoItem.firstChild);
+        }
+        todoItem.parentNode.removeChild(todoItem);
+    });
+
+    return todoItem;
 }
