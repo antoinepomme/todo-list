ae82834881bf6ac30fe05336b4cdbdee55acd743
antoinepomme <140323098+antoinepomme@users.noreply.github.com>
2023-08-18 12:19:32 +0200

    Dialog styling*

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import './styles/style.css';
 import { format, compareAsc } from 'date-fns';
 import todo from './modules/todo.js';
 
-
+const mainContainer = document.getElementById("main-container");
 const addButton = document.getElementById("add");
 const dialog = document.getElementById("dialog");
+const closeButton = document.getElementById("dialog-close");
 addButton.addEventListener('click', () => {
     dialog.showModal();
-})
+    mainContainer.style.filter = "blur(3px)";
+});
+closeButton.addEventListener('click', () => {
+    dialog.close();
+    mainContainer.style.filter = "blur(0px)";
+});
+dialog.addEventListener('cancel', () => {
+    mainContainer.style.filter = "blur(0px)";
+});
